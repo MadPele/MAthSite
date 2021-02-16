f9b7612ddf1fd76c5ba3b973c32cbdb56397d1be
Andrzej Derela <andrzejderela@gmail.com>
2021-02-16 16:40:22 +0100

    Add layout, home and about page

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Pele',
+        'title': '1st post',
+        'content': '1st post content',
+        'date_posted': 'February 6, 2021'
+    },
+    {
+        'author': 'MadPele',
+        'title': '2nd post',
+        'content': '2nd post content',
+        'date_posted': 'February 6, 2021'
+    }
+]
 
 
 @app.route('/')
 @app.route('/home')
 def home_page():
-    return 'Hello World5!'
+    return render_template('home.html', posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
